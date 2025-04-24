d585bed882b11ce78479acc3f67afa82eb212a51
Saad Inamdar <148623594+saad-inamdar7890@users.noreply.github.com>
2025-04-24 18:22:11 +0530

    cleaning

--- event-qr-scanner/data/valid-codes.js
+++ event-qr-scanner/data/valid-codes.js
@@ -1,10 +1,7 @@
-// This file defines the list of valid QR codes for the event
 
 const validCodes = [
   "EVENT2025-Guest001",
   "EVENT2025-Guest002",
   "EVENT2025-Guest003",
-  // Add all your event codes here
 ];
 
-// Don't use export here, we'll access this variable directly
